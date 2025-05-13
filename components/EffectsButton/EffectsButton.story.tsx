4d4f2dc17387609480a606487b9492cc56e89de3
John <54132759+RareDrops@users.noreply.github.com>
2025-05-13 10:55:41 +0000

    created test button component

---
+++ components/EffectsButton/EffectsButton.story.tsx
@@ -0,0 +1,7 @@
+import { EffectsButton } from './EffectsButton';
+
+export default {
+	title: 'EffectsButton',
+};
+
+export const Usage = () => <EffectsButton />;
---
+++ components/EffectsButton/EffectsButton.tsx
@@ -0,0 +1,14 @@
+import classes from './Effects.module.css';
+
+export function EffectsButton() {
+  return (
+    <button 
+      type="button" 
+      className={classes.button}
+      onClick={() => alert('Button has been clicked.')}
+      >
+      More
+    </button>
+  );
+}
+
